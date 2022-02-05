859465f3f4bf414e2a6fb9956f94fc2f54335217
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-02-05 12:16:27 -0300

    Implement async extraction

--- src/tasks/boredapi/extractor.py
+++ src/tasks/boredapi/extractor.py
@@ -1,18 +1,28 @@
-from time import sleep
+from concurrent.futures import as_completed
 
-from dagster import op
-from requests import get
+from dagster import op, repository
+from requests_futures.sessions import FuturesSession
 
 
 @op
 def extract_boredapi() -> list:
     ENDPOINT = 'https://www.boredapi.com/api/activity'
-    TOTAL = 100
-    LIMIT = 10
+    TOTAL = 1000
 
     data = []
+    futures = []
 
-    for _ in range(0, TOTAL, LIMIT):
-        data.extend([get(ENDPOINT).json() for _ in range(LIMIT)])
+    session = FuturesSession()
+
+    for i in range(0, TOTAL):
+        future = session.get(ENDPOINT)
+        future.i = i
+
+        futures.append(future)
+
+    for future in as_completed(futures):
+        response = future.result()
+
+        data.append(response.json())
 
     return data
--- src/tasks/boredapi/parser.py
+++ src/tasks/boredapi/parser.py
@@ -5,17 +5,19 @@ from src.database.models.activity import Activity
 
 @op
 def parse_boredapi(raw_data: list) -> list:
-    data = []
+    activities = []
 
-    for d in raw_data:
-        name = d['activity'].replace("'", "") or ''
-        kind = d['type'] or ''
-        participants = d['participants']
-        price = round(d['price'], 2)
+    unique_activities = [i for n, i in enumerate(raw_data) if i not in raw_data[n + 1:]]
+
+    for a in unique_activities:
+        name = a['activity'].replace("'", "") or ''
+        kind = a['type'] or ''
+        participants = a['participants']
+        price = round(a['price'], 2)
 
         activity = Activity(name=name, kind=kind, participants=participants,
                             price=price)
 
-        data.append(activity)
+        activities.append(activity)
 
-    return data
+    return activities
